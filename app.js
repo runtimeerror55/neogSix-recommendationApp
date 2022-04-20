5cc4633e1377e357a2121881b91f58b20cee8fdb
aakash deep <runtimeerror55@gmail.com>
2022-04-20 16:07:09 +0530

    changed a typo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let categories = document.querySelectorAll('.category')
 let output = document.querySelector('.output-Container')
 let arrayOfBookObjects = [
-    { 0: "javascript the definitive guide 4/5}", 1: "you dont know js 4/5", 2: "eloquent js 4/5" },
+    { 0: "javascript the definitive guide 4/5", 1: "you dont know js 4/5", 2: "eloquent js 4/5" },
     { 0: "theory of everything 4/5", 1: "the feyman lectures 4/5", 2: "relativity 4/5" },
     { 0: "atomic habits 4/5", 1: "psycology of money 4/5", 2: "rich dad poor dad 4/5" }
 ]
